cddcd7dad660365514a27edfe0f019ac5c60cd70
BruceW959 <75975657+BruceW959@users.noreply.github.com>
2021-06-12 06:05:46 +0800

    6:05

--- SUSTech-SQL-Project2-Public-master/src/main/java/Main.java
+++ SUSTech-SQL-Project2-Public-master/src/main/java/Main.java
@@ -37,54 +37,6 @@ public class Main {
         StudentService studentService = serviceFactory.createService(StudentService.class);
         studentService.enrollCourse(11710581,8);
 
-        //List<User> users = new LinkedList<>();
-        //users = userService.getAllUsers();
-        test(courseService);
-
-    }
-    public static void test(CourseService courseService){
-        Prerequisite calculus = new OrPrerequisite(List.of(
-                new CoursePrerequisite("MA101A"),
-                new CoursePrerequisite("MA101B")
-        ));
-        Prerequisite algebra = new CoursePrerequisite("MA103A");
-        Prerequisite prerequisite = new AndPrerequisite(List.of(calculus, algebra));
-        //courseService.addCourse("CS205","c++",3,64, Course.CourseGrading.HUNDRED_MARK_SCORE,prerequisite);
-        //courseService.addCourseSection("CS205",1,"中文一班",120);
-        List<Short> weekList=new LinkedList<>();
-        weekList.add((short)1);
-        weekList.add((short)3);
-        weekList.add((short)5);
-        weekList.add((short)6);
-        weekList.add((short)7);
-        int wl=0;
-        for (short s:weekList){
-            wl+=(int)Math.pow(2,s);
-        }
-        //courseService.addCourseSectionClass(9,1,DayOfWeek.MONDAY,weekList,(short) 1,(short)2,"teaching building 1");
-        //courseService.removeCourse("CS205");
-        //courseService.removeCourseSection(7);
-        //courseService.removeCourseSectionClass(1);
-        List<Course> cl=courseService.getAllCourses();
-        for(Course c:cl){
-            System.out.println(c.name+" "+c.id);
-        }
-        List<CourseSection> csl=courseService.getCourseSectionsInSemester("CS205",1);
-        for(CourseSection c:csl){
-            System.out.println(c.name+" "+c.id);
-        }
-        System.out.println(courseService.getCourseBySection(9).name);
-        System.out.println(courseService.getCourseSectionByClass(3).name);
-        List<User> users = new LinkedList<>();
-        users = userService.getAllUsers();*/
-        Prerequisite calculus = new OrPrerequisite(List.of(
-                new CoursePrerequisite("MA101A"),
-                new CoursePrerequisite("MA101B")
-        ));
-        Prerequisite algebra = new CoursePrerequisite("MA103A");
-        Prerequisite prerequisite = new AndPrerequisite(List.of(calculus, algebra));
-        ReferenceCourseService r=new ReferenceCourseService();
-        System.out.println(r.addPre(prerequisite));
     }
 
 }
